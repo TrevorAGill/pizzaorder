8aaf0b7423aa7916a3174e9e3c3a55991f227e02
Trevor Gill <trevor.a.gill@gmail.com>
2017-07-22 08:09:01 -0700

    add constructor for pizza object and prototype for returning price based on pizza size (no toppings)

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       var inputtedToppings = $(this).val();
     }
     var inputtedSize = $("select#size").val();
-
+    $("#result").text()
 
 //Backend
     var newPizza = new Pizza(inputtedToppings,inputtedSize);
@@ -18,10 +18,43 @@ $(document).ready(function(){
 
 
     Pizza.prototype.Size = function() {
+      var pizzaPrice = 0;
       if(this.size === "small") {
-        return 8;
+        var pizzaPrice = 8;
+        return Toppings(pizzaPrice);
       } else if (this.size === "medium") {
-        return 10;
+        var pizzaPrice = 10;
+        return Toppings(pizzaPrice);
       } else if (this.size === "large") {
-        return 12;
+        var pizzaPrice = 12;
+        return Toppings(pizzaPrice);
     }
+
+
+    Pizza.prototype.Toppings = function() {
+      alert((this.toppings.length) * 1.5);
+      return (this.toppings.length) * 1.5;
+
+    }
+
+    Ticket.prototype.FinalPrice = function() {
+      return 0 + this.Age() + this.Showtime();
+    }
+
+
+
+    Ticket.prototype.receipt = function() {
+    return this.movie + " " + this.showtime + " " + this.age + " " + this.finalPrice();
+    }
+
+
+    var inputtedAge = 0;
+    var inputtedMovie = "";
+    var inputtedShowtime = "";
+    var ticketPrice = 0;
+
+
+    $("#result").text(currentTicket.FinalPrice())
+  event.preventDefault();
+  });
+});
