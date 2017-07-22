f15517244948555a044a26258851d194486f3d91
Trevor Gill <trevor.a.gill@gmail.com>
2017-07-22 09:20:54 -0700

    add javascript to return a final price based on pizza size AND number of toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,60 +1,39 @@
 //Frontend
 $(document).ready(function(){
   $("form#pizza-selection").submit(function(event) {
-
+    toppingsArray = [];
     $("input:checkbox[name=toppings]:checked").each(function(){
-      var inputtedToppings = $(this).val();
-    }
+      toppingsArray.push($(this).val());
+    });
     var inputtedSize = $("select#size").val();
-    $("#result").text()
-
-//Backend
-    var newPizza = new Pizza(inputtedToppings,inputtedSize);
-
-    function Pizza(toppings, size) {
-      this.toppings = toppings;
-      this.size = size;
-    }
-
-
-    Pizza.prototype.Size = function() {
-      var pizzaPrice = 0;
-      if(this.size === "small") {
-        var pizzaPrice = 8;
-        return Toppings(pizzaPrice);
-      } else if (this.size === "medium") {
-        var pizzaPrice = 10;
-        return Toppings(pizzaPrice);
-      } else if (this.size === "large") {
-        var pizzaPrice = 12;
-        return Toppings(pizzaPrice);
-    }
-
-
-    Pizza.prototype.Toppings = function() {
-      alert((this.toppings.length) * 1.5);
-      return (this.toppings.length) * 1.5;
-
-    }
-
-    Ticket.prototype.FinalPrice = function() {
-      return 0 + this.Age() + this.Showtime();
-    }
-
-
-
-    Ticket.prototype.receipt = function() {
-    return this.movie + " " + this.showtime + " " + this.age + " " + this.finalPrice();
-    }
-
-
-    var inputtedAge = 0;
-    var inputtedMovie = "";
-    var inputtedShowtime = "";
-    var ticketPrice = 0;
-
-
-    $("#result").text(currentTicket.FinalPrice())
-  event.preventDefault();
+    var newPizza = new Pizza(toppingsArray,inputtedSize);
+    $("#result").text(newPizza.FinalPrice(toppingsArray,inputtedSize));
+    event.preventDefault();
   });
 });
+
+//Backend
+
+function Pizza(toppings, size) {
+  this.toppings = toppings;
+  this.size = size;
+}
+
+
+Pizza.prototype.Size = function() {
+  if(this.size === "small") {
+    return 8;
+  } else if (this.size === "medium") {
+    return 10;
+  } else if (this.size === "large") {
+    return 12;
+  }
+}
+
+Pizza.prototype.Toppings = function(toppingsArray,inputtedSize) {
+  return (this.toppings.length) * 1.5;
+}
+
+Pizza.prototype.FinalPrice = function() {
+  return "Your total comes to $" + (0 + this.Size() + this.Toppings()).toFixed(2);
+}
